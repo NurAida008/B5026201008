f06b09fca80ee24ec8f3fc850b757409b0eea2fa
NurAida008 <aida.nurfebrianti@gmail.com>
2021-11-15 15:49:35 +0700

    Database
    Database Pegawai

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,11 @@ Route:: get('tugas4', function(){
 
 Route::get('ets','ViewController@showets');
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
