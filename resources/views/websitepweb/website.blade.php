33fe629a2b192f7096a8141ea21d3261335defee
NurAida008 <aida.nurfebrianti@gmail.com>
2021-12-04 15:57:39 +0700

    Update Website
    Update tugas website

--- resources/views/websitepweb/website.blade.php
+++ resources/views/websitepweb/website.blade.php
@@ -11,7 +11,6 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dashboard/">
 
 
-
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-datetimepicker.min.css') }}">
@@ -49,12 +48,7 @@
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
-    </div>
-  </div>
+
 </header>
 
 <div class="container-fluid">
@@ -68,7 +62,7 @@
             </div>
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/pegawai">
+            <a class="nav-link" aria-current="page" href="/pegawai">
               <span data-feather="home"></span>
               Pegawai
             </a>
@@ -85,16 +79,25 @@
               Mutasi
             </a>
           </li>
-        </ul>
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <a class="link-secondary" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
-
+          <li class="nav-item" style="margin-top: 230px">
+            <a class="nav-link" href="/mutasi">
+              <span data-feather="shopping-cart"></span>
+              Copyright &copy;
+              <script>
+                document.write(new Date().getFullYear());
+              </script>
+               oleh 5026201008
+               Nur Aida Febrianti
+            </a>
+          </li>
         </ul>
+        <div class="footer" style="font-family:Arial, Helvetica, sans-serif; margin-top: 100px" >
+            <p>
+
+            </p>
+          </div>
+
       </div>
     </nav>
 
